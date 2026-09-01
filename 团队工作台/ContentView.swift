485808fae240bc798727ce9bb291f2bcea1e7680
Beauty <liang_wu@apple.com>
2026-09-01 17:28:37 +0800

    Initial Commit

---
+++ 团队工作台/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  团队工作台
+//
+//  Created by 靓靓 on 2026/9/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
